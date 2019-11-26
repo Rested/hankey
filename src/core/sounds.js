191609d3542a6399efb45937f3949c66beaf804b
Reuben Thomas-Davis <reubenestd@gmail.com>
2019-11-26 09:21:38 +0000

    add sound, fix bugs

---
+++ src/core/sounds.js
@@ -0,0 +1,21 @@
+import { allKoreanCharacters } from "./keys"
+import Hangul from 'hangul-js';
+
+export const soundMapping = (()=> {
+    const mapping = {};
+    allKoreanCharacters.forEach(char => {
+        if (char === 'ㅇ'){
+            mapping['ㅇ'] = '이응'
+        }else{
+            mapping[char] = Hangul.isVowel(char) ? Hangul.assemble(['ㅇ', char]) : Hangul.assemble([char, 'ㅡ']);
+        }
+    })
+    return mapping;
+})() 
+
+export function playSound(character){
+    const utterance = new SpeechSynthesisUtterance(soundMapping[character]);
+    utterance.lang = 'ko-KR'
+    utterance.rate = 0.6
+    speechSynthesis.speak(utterance)
+}

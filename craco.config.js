5bed28ec1edee9ea7d3fa1536d89e191f709ad39
Reuben Thomas-Davis <reubenestd@gmail.com>
2019-12-18 14:56:25 +0000

    :wrench: Set craco config to use babel

--- craco.config.js
+++ craco.config.js
@@ -2,4 +2,9 @@ module.exports = {
     plugins: [{
         plugin: require('@semantic-ui-react/craco-less')
     }],
+    babel: {
+        loaderOptions: {
+            babelrc: true,
+        },
+    },
 }
